5e438c6fcda680446c723817db0e3eaf5df3f8ca
ALEXANDROS CHRISTINAKIS <csd4764@csd.uoc.gr>
2023-03-12 20:19:02 +0200

    build(cmake): use separate file for `flex`

---
+++ cmake/flex-scanner.cmake
@@ -0,0 +1,12 @@
+find_package(FLEX 2.5.13)
+
+flex_target(FlexTarget
+    # Source file
+    ${CMAKE_SOURCE_DIR}/src/alpha/lex/scanner.l
+
+    # Output (c/cpp) source file
+    ${CMAKE_SOURCE_DIR}/src/alpha/lex/scanner.c
+
+    # Output header file
+    DEFINES_FILE ${CMAKE_SOURCE_DIR}/include/alpha/lex/scanner.h
+)
