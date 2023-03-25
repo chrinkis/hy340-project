342d571e38fe458c901e3d7f08ce88f0234c3c8c
ALEXANDROS CHRISTINAKIS <csd4764@csd.uoc.gr>
2023-03-25 10:43:18 +0200

    build: support tests through `compile.sh`

--- compile.sh
+++ compile.sh
@@ -1,4 +1,9 @@
+if [[ $1 = "-t" ]]; then
+    echo "This may take a while. Please be patient..."
+    CMAKE_ARGS="-Dskip_tests:BOOL=OFF"
+fi
+
 mkdir -p build &&
     cd build &&
-    cmake .. &&
-    make al
+    cmake ${CMAKE_ARGS} .. &&
+    make all
