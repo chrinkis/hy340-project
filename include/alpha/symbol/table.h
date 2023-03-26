d2447e9014d44087d1105feb239be9a1a8faf881
ALEXANDROS CHRISTINAKIS <csd4764@csd.uoc.gr>
2023-03-26 15:47:00 +0300

    feat(symbol): add missing `const`s keywords

--- include/alpha/symbol/table.h
+++ include/alpha/symbol/table.h
@@ -18,17 +18,17 @@ class Table {
   void increase_scope();
   void decrease_scope();
 
-  bool symbol_is_visible(const std::string& name);
+  bool symbol_is_visible(const std::string& name) const;
 
-  bool can_add_variable(const std::string& name);
+  bool can_add_variable(const std::string& name) const;
   void add_variable(const std::string& name);
 
-  bool can_add_function(const std::string& name);
+  bool can_add_function(const std::string& name) const;
   void start_function();
   void start_function(const std::string& name);
   void end_function();
 
-  void can_add_argument(const std::string& name);
+  void can_add_argument(const std::string& name) const;
   void add_argument(const std::string& name);
   void add_last_argument(const std::string& name);
 };
