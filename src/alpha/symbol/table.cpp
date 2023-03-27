05f8c63db70eecc7ce4390f5295a7c7e7aee1b8a
ALEXANDROS CHRISTINAKIS <csd4764@csd.uoc.gr>
2023-03-27 18:29:00 +0300

    fix(symbol): hide symbols in Table::decrease_scope

--- src/alpha/symbol/table.cpp
+++ src/alpha/symbol/table.cpp
@@ -44,11 +44,15 @@ void Table::increase_scope() {
 void Table::decrease_scope() {
   Symbol::Scope prev = this->current_scope;
 
+  for (auto pair : this->symbol_map) {  // TODO slow peformance
+    if (pair.second.get_symbol()->get_scope() == this->current_scope) {
+      pair.second.deactivate();
+    }
+  }
+
   this->current_scope--;
 
   assert(this->current_scope < prev);
-
-  // FIXME hide symbols
 }
 
 Table::SearchResult Table::search_for_visible_symbol(
