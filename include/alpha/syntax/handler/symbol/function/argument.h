f9bc38641e418e773476ba6ae030de5b53b0cde5
ALEXANDROS CHRISTINAKIS <csd4764@csd.uoc.gr>
2023-04-27 22:41:30 +0300

    feat(syntax): add handlers' api

--- include/alpha/syntax/handler/symbol/function/argument.h
+++ include/alpha/syntax/handler/symbol/function/argument.h
@@ -1,5 +1,22 @@
 #pragma once
 
+#include <alpha/syntax/manager/terminal/identifier.h>
+
+namespace alpha {
+namespace syntax {
+namespace handlers {
+namespace symbol {
+namespace function {
+
+void add_arg(manager::terminal::Identifier& argument);
+void end_arg_list();
+
+}  // namespace function
+}  // namespace symbol
+}  // namespace handlers
+}  // namespace syntax
+}  // namespace alpha
+
 #define SET_COLOR_FOR_ERROR "\033[31m"
 #define RESET_COLOR "\033[0m"
 
--- include/alpha/syntax/handler/symbol/function/definition.h
+++ include/alpha/syntax/handler/symbol/function/definition.h
@@ -1,5 +1,24 @@
 #pragma once
 
+#include <alpha/syntax/manager/terminal/function.h>
+#include <alpha/syntax/manager/terminal/identifier.h>
+
+namespace alpha {
+namespace syntax {
+namespace handlers {
+namespace symbol {
+namespace function {
+
+void start(manager::terminal::Identifier& identifier);
+void start(manager::terminal::Function& function);
+void end();
+
+}  // namespace function
+}  // namespace symbol
+}  // namespace handlers
+}  // namespace syntax
+}  // namespace alpha
+
 #define SET_COLOR_FOR_ERROR "\033[31m"
 #define RESET_COLOR "\033[0m"
 
