d3b128c372b33fe6f3501063cc55c26d15d186de
ALEXANDROS CHRISTINAKIS <csd4764@csd.uoc.gr>
2023-04-28 22:10:36 +0300

    feat(syntax): add methods for `holder`s

--- include/alpha/syntax/holder/Symbol.h
+++ include/alpha/syntax/holder/Symbol.h
@@ -1,13 +1,23 @@
 #pragma once
 
-#include <alpha/symbol/table.h>
+#include <alpha/symbol/table_manager.h>
+
+#include <optional>
 
 namespace alpha {
 namespace syntax {
 namespace holder {
 
-struct Symbol {
-  symbol::Table::SearchResult result;
+class Symbol {
+ public:
+  using Optional = std::optional<symbol::Symbol::SharedPtr>;
+
+ private:
+  Optional symbol_opt;
+
+ public:
+  Optional get_symbol() const;
+  void set_symbol(const Optional& optional_symbol);
 };
 
 }  // namespace holder
