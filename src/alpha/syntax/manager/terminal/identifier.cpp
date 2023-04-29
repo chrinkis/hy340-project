230be88e12ea8e4761c8fce8b3a5a10640ef2bc9
NIKOLAOS GRIGOROUDIS <csd4619@csd.uoc.gr>
2023-04-29 14:52:59 +0300

    feat(syntax): implement 'identifier' manager

---
+++ src/alpha/syntax/manager/terminal/identifier.cpp
@@ -0,0 +1,17 @@
+#include <alpha/syntax/location.h>
+#include <alpha/syntax/manager/terminal/identifier.h>
+
+using namespace alpha::syntax::manager::terminal;
+
+using Location = alpha::syntax::location;
+
+Identifier::Identifier(const std::string& name, const Location& location)
+    : name(name), location(location) {}
+
+std::string Identifier::get_name() const {
+  return this->name;
+}
+
+Location Identifier::get_location() const {
+  return this->location;
+}
