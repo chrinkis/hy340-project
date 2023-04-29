efe22f8390a72cac72d7970357bc6098a0dfa3ed
ALEXANDROS CHRISTINAKIS <csd4764@csd.uoc.gr>
2023-04-29 14:45:23 +0300

    feat(syntax): force description in error message

--- include/alpha/syntax/handler/symbol/function/error_checker.h
+++ include/alpha/syntax/handler/symbol/function/error_checker.h
@@ -10,7 +10,6 @@ namespace handlers {
 namespace symbol {
 namespace function {
 
-void check_for_errors(holder::Symbol& symbol_holder);
 void check_for_errors(holder::Symbol& symbol_holder,
                       const std::string& description);
 
