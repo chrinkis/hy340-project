65376aa0d654f406da8770b192407f9e14d5c7dd
NIKOLAOS GRIGOROUDIS <csd4619@csd.uoc.gr>
2023-04-30 15:21:36 +0300

    refactor(syntax): remove unneeded `include`s

--- src/alpha/syntax/manager/nonterminal/assignexpr.cpp
+++ src/alpha/syntax/manager/nonterminal/assignexpr.cpp
@@ -1,8 +1,6 @@
 #include <alpha/syntax/handler/symbol/function/error_checker.h>
 #include <alpha/syntax/handler/symbol/symbol.h>
 #include <alpha/syntax/manager/nonterminal/assignexpr.h>
-#include <alpha/syntax/manager/nonterminal/lvalue.h>
-#include "alpha/syntax/holder/Symbol.h"
 
 using namespace alpha::syntax::manager::nonterminal;
 
