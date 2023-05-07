2bfc2ba420becc729d829c9ee83f4fa3c6d1448f
ALEXANDROS CHRISTINAKIS <csd4764@csd.uoc.gr>
2023-05-07 13:42:54 +0300

    feat(syntax): add error handling with codes

--- include/alpha/syntax/error.h
+++ include/alpha/syntax/error.h
@@ -3,8 +3,43 @@
 #include <alpha/syntax/parser.h>
 
 #include <string>
+#include "alpha/symbol/symbol.h"
 
 namespace alpha::syntax::error {
+
+enum Type {
+  UNDEFINED_REFFERENCE_TO_GLOBAL_VAR,
+  INACCESSIBLE_REFFERENCE_TO_VAR,
+  LOCAL_VAR_SHADOWS_LIB_FUNCTION,
+  INVALID_ARGUMNET_NAME,
+  INVALID_NAME_FOR_FUNC_DEFINITION,
+  INVALID_FUNCTION_OPERATION,
+};
+
+enum class Operator {
+  ASSIGN,
+  PLUS,
+  MINUS,
+  STAR,
+  DIV,
+  MOD,
+  GREATER,
+  GREATER_EQUAL,
+  LESS,
+  LESS_EQUAL,
+  EQUAL,
+  NOT_EQUAL,
+  AND,
+  OR,
+  DOT,
+  SQUARE_BRACKETS,
+  UNARY_MINUS,
+  NOT,
+  PLUS_PLUS_PRE,     // ++foo
+  PLUS_PLUS_POST,    // foo++
+  MINUS_MINUS_PRE,   // --foo
+  MINUS_MINUS_POST,  // foo--
+};
 
 void print(const std::string& message,
            const alpha::syntax::Parser::location_type& location);
@@ -13,5 +48,9 @@ void print_semantic(const std::string& message,
                     const alpha::syntax::Parser::location_type& location);
 
 void print_semantic(const std::string& message);
+
+void print(Type err_type,
+           Operator op,
+           const alpha::symbol::Symbol::SharedPtr& symbol);
 
 }  // namespace alpha::syntax::error
--- src/alpha/syntax/error.cpp
+++ src/alpha/syntax/error.cpp
@@ -1,5 +1,6 @@
 #include <alpha/syntax/error.h>
 
+#include <cassert>
 #include <iostream>
 #include <string>
 
@@ -7,6 +8,8 @@
 #define DEFAULT_COLOR "\033[0m"
 
 namespace alpha::syntax::error {
+
+static std::string to_string(Operator op);
 
 void print(const std::string& message,
            const alpha::syntax::Parser::location_type& location) {
@@ -27,6 +30,79 @@ void print_semantic(const std::string& message,
 void print_semantic(const std::string& message) {
   std::cerr << ERROR_COLOR << "semantic error, " << message << std::endl
             << DEFAULT_COLOR;
+}
+
+void print(Type err_type,
+           Operator op,
+           const alpha::symbol::Symbol::SharedPtr& symbol) {
+  switch (err_type) {
+    case UNDEFINED_REFFERENCE_TO_GLOBAL_VAR:
+    case INACCESSIBLE_REFFERENCE_TO_VAR:
+      assert(0);  // FIXME
+    case LOCAL_VAR_SHADOWS_LIB_FUNCTION:
+      assert(0);  // FIXME
+    case INVALID_ARGUMNET_NAME:
+      assert(0);  // FIXME
+    case INVALID_NAME_FOR_FUNC_DEFINITION:
+      assert(0);  // FIXME
+    case INVALID_FUNCTION_OPERATION:
+      print_semantic("cannot perform `" + to_string(op) + "` with function `" +
+                     symbol->get_name() + "` as operand");
+      break;
+    default:
+      assert(0);
+  }
+}
+
+static std::string to_string(Operator op) {
+  switch (op) {
+    case Operator::ASSIGN:
+      return "=";
+    case Operator::PLUS:
+      return "+";
+    case Operator::MINUS:
+      return "-";
+    case Operator::STAR:
+      return "*";
+    case Operator::DIV:
+      return "/";
+    case Operator::MOD:
+      return "%";
+    case Operator::GREATER:
+      return ">";
+    case Operator::GREATER_EQUAL:
+      return ">=";
+    case Operator::LESS:
+      return "<";
+    case Operator::LESS_EQUAL:
+      return "<=";
+    case Operator::EQUAL:
+      return "==";
+    case Operator::NOT_EQUAL:
+      return "!=";
+    case Operator::AND:
+      return "and";
+    case Operator::OR:
+      return "or";
+    case Operator::DOT:
+      return ".";
+    case Operator::SQUARE_BRACKETS:
+      return "[]";
+    case Operator::UNARY_MINUS:
+      return "-";
+    case Operator::NOT:
+      return "!";
+    case Operator::PLUS_PLUS_PRE:
+      return "++ (pre)";
+    case Operator::PLUS_PLUS_POST:
+      return "++ (post)";
+    case Operator::MINUS_MINUS_PRE:
+      return "-- (pre)";
+    case Operator::MINUS_MINUS_POST:
+      return "-- (post)";
+    default:
+      assert(0);
+  }
 }
 
 }  // namespace alpha::syntax::error
