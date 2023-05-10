4c8b489f0dc3e2be9e05a5649ccca72c119aabea
NIKOLAOS GRIGOROUDIS <csd4619@csd.uoc.gr>
2023-05-10 18:58:01 +0300

    feat(syntax): add `Stmt` manager API

---
+++ include/alpha/syntax/manager/nonterminal/stmt.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <alpha/icode/quad/quad.h>
+
+namespace alpha {
+namespace syntax {
+namespace manager {
+namespace nonterminal {
+
+class Breakstmt;
+class Continuestmt;
+
+class Stmt {
+ public:
+  using Quad = alpha::icode::quad::Quad;
+
+ private:
+  Quad::Label breaklist;
+  Quad::Label contlist;
+
+ public:
+  static Stmt from_expr_smclnTkn();
+  static Stmt from_ifstmt();
+  static Stmt from_whilestmt();
+  static Stmt from_forstmt();
+  static Stmt from_returnstmt();
+  static Stmt from_breakstmt(const Breakstmt& breakstmt);
+  static Stmt from_continuestmt(const Continuestmt& continuestmt);
+  static Stmt from_block();
+  static Stmt from_funcdef();
+  static Stmt from_smclnTkn();
+
+ public:
+  Quad::Label get_breaklist() const;
+  void set_breaklist(const Quad::Label& breaklist);
+  Quad::Label get_contlist() const;
+  void set_contlist(const Quad::Label& contlist);
+};
+
+}  // namespace nonterminal
+}  // namespace manager
+}  // namespace syntax
+}  // namespace alpha
