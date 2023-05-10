a657239fb1d2451e94666c3679164aff172028a4
ALEXANDROS CHRISTINAKIS <csd4764@csd.uoc.gr>
2023-05-10 15:00:28 +0300

    feat(syntax): update `Funcprefix` manager API

--- include/alpha/syntax/manager/nonterminal/funcprefix.h
+++ include/alpha/syntax/manager/nonterminal/funcprefix.h
@@ -1,21 +1,36 @@
 #pragma once
 
+#include <alpha/icode/quad/quad.h>
+#include <alpha/symbol/symbol.h>
 #include <alpha/syntax/manager/terminal/function.h>
 #include <alpha/syntax/manager/terminal/identifier.h>
-
-#include <alpha/syntax/holder/Symbol.h>
 
 namespace alpha {
 namespace syntax {
 namespace manager {
 namespace nonterminal {
 
-class Funcprefix : public holder::Symbol {
+class Funcprefix {
+ public:
+  using Quad = icode::quad::Quad;
+  using Symbol = symbol::Symbol;
+
+ private:
+  Quad::Label quad_label;
+  Symbol::SharedPtr symbol;
+
  public:
   static Funcprefix from_functionTkn(const terminal::Function& function);
 
   static Funcprefix from_functionTkn_identifierTkn(
       const terminal::Identifier& identifier);
+
+ public:
+  Quad::Label get_quad_label() const;
+  void set_quad_label(const Quad::Label& quad_label);
+
+  Symbol::SharedPtr get_symbol() const;
+  void set_symbol(const Symbol::SharedPtr& symbol);
 };
 
 }  // namespace nonterminal
