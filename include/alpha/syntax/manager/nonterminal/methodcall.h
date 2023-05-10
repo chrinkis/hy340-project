58a9fcf60e11549c21193fd2662f6946f425f751
ALEXANDROS CHRISTINAKIS <csd4764@csd.uoc.gr>
2023-05-10 16:09:28 +0300

    feat(syntax): add `Methodcall` manager API

---
+++ include/alpha/syntax/manager/nonterminal/methodcall.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <alpha/syntax/manager/nonterminal/expr.h>
+#include <alpha/syntax/manager/terminal/identifier.h>
+
+#include <vector>
+
+namespace alpha {
+namespace syntax {
+namespace manager {
+namespace nonterminal {
+
+class Elist;
+
+class Methodcall {
+ public:
+  using ExprCollection = std::vector<Expr>;
+
+ private:
+  ExprCollection elist;
+  bool method;
+  terminal::Identifier id;
+
+ public:
+  static Methodcall from_doubleDotTkn_identifier_lParTkn_elist_rParTkn(
+      const terminal::Identifier& id,
+      const Elist& elist);
+
+ public:
+  ExprCollection get_elist() const;
+  void set_elist(const ExprCollection& elist);
+
+  bool is_method() const;
+  bool set_method(bool is_method);
+
+  terminal::Identifier get_id() const;
+  void set_id(const terminal::Identifier& id);
+};
+
+}  // namespace nonterminal
+}  // namespace manager
+}  // namespace syntax
+}  // namespace alpha
