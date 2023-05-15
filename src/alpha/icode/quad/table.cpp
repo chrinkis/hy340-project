51a5d79b29150d49df7e9d92b8b97d7e38c0f267
ALEXANDROS CHRISTINAKIS <csd4764@csd.uoc.gr>
2023-05-15 11:35:45 +0300

    feat(icode): add `emit` for each opcode

--- src/alpha/icode/quad/table.cpp
+++ src/alpha/icode/quad/table.cpp
@@ -6,6 +6,110 @@ void Table::emit(const Quad& quad) {
   this->table.push_back(quad);
 }
 
+void Table::emit_assign() {
+  ;
+}
+
+void Table::emit_add() {
+  ;
+}
+
+void Table::emit_sub() {
+  ;
+}
+
+void Table::emit_mul() {
+  ;
+}
+
+void Table::emit_div() {
+  ;
+}
+
+void Table::emit_mod() {
+  ;
+}
+
+void Table::emit_uminus() {
+  ;
+}
+
+void Table::emit_and() {
+  ;
+}
+
+void Table::emit_or() {
+  ;
+}
+
+void Table::emit_not() {
+  ;
+}
+
+void Table::emit_if_eq() {
+  ;
+}
+
+void Table::emit_if_noteq() {
+  ;
+}
+
+void Table::emit_if_lesseq() {
+  ;
+}
+
+void Table::emit_if_greatereq() {
+  ;
+}
+
+void Table::emit_if_less() {
+  ;
+}
+
+void Table::emit_if_greater() {
+  ;
+}
+
+void Table::emit_jump() {
+  ;
+}
+
+void Table::emit_call() {
+  ;
+}
+
+void Table::emit_param() {
+  ;
+}
+
+void Table::emit_ret() {
+  ;
+}
+
+void Table::emit_getretval() {
+  ;
+}
+
+void Table::emit_funcstart() {
+  ;
+}
+
+void Table::emit_funcend() {
+  ;
+}
+
+void Table::emit_tablecreate() {
+  ;
+}
+
+void Table::emit_tablegetelem() {
+  ;
+}
+
+void Table::emit_tablesetelem() {
+  ;
+}
+
 std::ostream& operator<<(std::ostream& os, const Table& qt) {
   os << std::left;
 
