64140be3333c659e5c9a5bbf9aa6e4275d6bfa93
ALEXANDROS CHRISTINAKIS <csd4764@csd.uoc.gr>
2023-05-17 23:06:04 +0300

    fix(syntax): implement missing methods

--- src/alpha/syntax/manager/nonterminal/continuestmt.cpp
+++ src/alpha/syntax/manager/nonterminal/continuestmt.cpp
@@ -6,6 +6,7 @@
 
 using namespace alpha::syntax::manager::nonterminal;
 using Opcode = alpha::icode::quad::Quad::Opcode;
+using Quad = alpha::icode::quad::Quad;
 
 Continuestmt::Continuestmt() : break_list_head(0), continue_list_head(0) {}
 
@@ -22,4 +23,20 @@ Continuestmt Continuestmt::from_continueTkn(const location& loc) {
   }
 
   return continuestmt;
+}
+
+Quad::Label Continuestmt::get_break_list_head() const {
+  return this->break_list_head;
+}
+
+void Continuestmt::set_break_list_head(const Quad::Label& label) {
+  this->break_list_head = label;
+}
+
+Quad::Label Continuestmt::get_continue_list_head() const {
+  return this->continue_list_head;
+}
+
+void Continuestmt::set_continue_list_head(const Quad::Label& label) {
+  this->continue_list_head = label;
 }
