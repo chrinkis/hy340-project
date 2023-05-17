dd4fb7d840389aec4e894f9cb6e115e805bcb53d
ALEXANDROS CHRISTINAKIS <csd4764@csd.uoc.gr>
2023-05-17 11:25:27 +0300

    fix(icode): change return type of Table::new_list

--- include/alpha/icode/quad/table.h
+++ include/alpha/icode/quad/table.h
@@ -64,7 +64,7 @@ class Table {
 
   void patch_list(const Quad::Line& list_head, const Quad::Label& label);
 
-  Quad& new_list(const Quad::Line& start);
+  Quad::Line new_list(const Quad::Line& start);
 
   Quad::Line merge_lists(const Quad::Line& list_head_a,
                          const Quad::Line& list_head_b);
