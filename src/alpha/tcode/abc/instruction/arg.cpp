03c88e9e4b58641a1174d92d5f92c092eb379f34
NIKOLAOS GRIGOROUDIS <csd4619@csd.uoc.gr>
2023-06-01 22:57:17 +0300

    feat(tcode): implement Arg::value_of_program_func

--- src/alpha/tcode/abc/instruction/arg.cpp
+++ src/alpha/tcode/abc/instruction/arg.cpp
@@ -1,6 +1,8 @@
 #include <alpha/tcode/abc/instruction/arg.h>
 
+#include <alpha/symbol/function.h>
 #include <alpha/tcode/abc/consts/consts.h>
+#include <alpha/tcode/abc/consts/user_func.h>
 #include <utils/warnings.h>
 #include <cassert>
 
@@ -48,4 +50,16 @@ Arg::Type Arg::type_of_var(const symbol::Symbol& symbol) {
       assert(0);
   }
 }
+
+unsigned Arg::value_of_program_func(const symbol::Symbol& symbol) {
+  const symbol::Function* function =
+      dynamic_cast<const symbol::Function*>(&symbol);
+  assert(function);
+
+  consts::UserFunc user_func(function->get_iaddress(), function->get_name(),
+                             function->get_total_locals());
+
+  return tcodeConsts.user_func(user_func);
+}
+
 }  // namespace alpha::tcode::abc::instruction
