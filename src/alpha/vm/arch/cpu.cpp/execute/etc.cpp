41f18b055f582be5f9bad9e723cce847694a4578
ALEXANDROS CHRISTINAKIS <csd4764@csd.uoc.gr>
2023-06-07 10:07:13 +0300

    feat(vm): implement `Cpu::execute_assign`

--- src/alpha/vm/arch/cpu.cpp/execute/etc.cpp
+++ src/alpha/vm/arch/cpu.cpp/execute/etc.cpp
@@ -5,7 +5,14 @@
 namespace alpha::vm::arch::cpu {
 
 void Cpu::execute_assign(const AbcInstruction& instr) {
-  WARN_EMPTY_FUNC_IMPL();
+  mem::Cell& lv = this->translate_arg_to_cell(instr.get_result());
+  mem::Cell& rv =
+      this->translate_arg_to_cell(instr.get_arg_a(), this->registers.arg_a);
+
+  FIXME;  // add missing assertions for vl
+  FIXME;  // add missing assertions for rv
+
+  this->assign(lv, rv);
 }
 
 void Cpu::execute_nop(const AbcInstruction& instr) {
