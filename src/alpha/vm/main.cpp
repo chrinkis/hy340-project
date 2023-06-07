3ff58ccc5af49ebb898a1d9aa4cbabbe20ad3f4a
NIKOLAOS GRIGOROUDIS <csd4619@csd.uoc.gr>
2023-06-07 17:53:16 +0300

    feat(vm): use parser in main

--- src/alpha/vm/main.cpp
+++ src/alpha/vm/main.cpp
@@ -1,3 +1,24 @@
+#include <alpha/vm/arch/mem/code/table.h>
+#include <alpha/vm/arch/mem/consts/consts.h>
+#include <alpha/vm/parser/parser.h>
+
+#include <iostream>
+
+using namespace vm;
+
+using Parser = parser::Parser;
+using CodeTable = alpha::vm::arch::mem::code::Table;
+
 int main() {
+  CodeTable code_table;
+  Parser parser(vmConsts, code_table);
+
+  try {
+    parser.parse("tcode.txt");
+  } catch (std::runtime_error e) {
+    std::cerr << e.what() << std::endl;
+    exit(1);
+  }
+
   return 0;
 }
