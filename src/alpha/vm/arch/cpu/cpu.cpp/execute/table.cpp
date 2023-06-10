1e0475bf682e166e0bd1c3d62a78c7ed005ce9dd
ALEXANDROS CHRISTINAKIS <csd4764@csd.uoc.gr>
2023-06-10 18:26:22 +0300

    feat(vm): implement `Cpu::execute_tablegetelem`

--- src/alpha/vm/arch/cpu/cpu.cpp/execute/table.cpp
+++ src/alpha/vm/arch/cpu/cpu.cpp/execute/table.cpp
@@ -1,5 +1,7 @@
 #include <alpha/vm/arch/cpu/cpu.h>
 
+#include <alpha/vm/runtime/messages/error.h>
+
 #include <utils/warnings.h>
 
 using RunTimeTable = alpha::vm::runtime::table::Table;
@@ -18,7 +20,42 @@ void Cpu::execute_newtable(const AbcInstruction& instr) {
 }
 
 void Cpu::execute_tablegetelem(const AbcInstruction& instr) {
-  WARN_EMPTY_FUNC_IMPL();
+  mem::Cell& lv = this->translate_arg_to_cell(instr.get_result());
+
+  const mem::Cell& table = this->translate_arg_to_cell(instr.get_arg_a());
+
+  const mem::Cell& index =
+      this->translate_arg_to_cell(instr.get_arg_b(), this->registers.arg_a);
+
+  FIXME;  // add missing assertS
+
+  lv.clear();
+  lv = mem::Cell::for_nil();  // default value
+
+  if (table.get_type() != mem::Cell::Type::TABLE) {
+    runtime::messages::error("illdegal use of type `" +
+                             table.get_type_as_string() + "` as table!");
+    this->execution_finished = true;
+
+    return;
+  }
+
+  if (index.get_type() == mem::Cell::Type::UNDEF ||
+      index.get_type() == mem::Cell::Type::NIL) {
+    runtime::messages::error("illegal type `" + table.get_type_as_string() +
+                             "` for table-index!");
+    this->execution_finished = true;
+
+    return;
+  }
+
+  try {
+    const mem::Cell& value = table.get_table().get_element(index);
+
+    this->assign(lv, value);
+  } catch (const std::invalid_argument& err) {
+    runtime::messages::warning(err.what());
+  }
 }
 
 void Cpu::execute_tablesetelem(const AbcInstruction& instr) {
