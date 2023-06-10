059acfe81ac296f494704cbbf83f364d2f3457bc
NIKOLAOS GRIGOROUDIS <csd4619@csd.uoc.gr>
2023-06-10 17:14:29 +0300

    feat(vm): get source file name as `main` argument

--- src/alpha/vm/main.cpp
+++ src/alpha/vm/main.cpp
@@ -12,12 +12,23 @@ using Memory = arch::mem::Memory;
 using Cpu = arch::cpu::Cpu;
 using LibFuncs = runtime::libint::LibFunctions;
 
-int main() {
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    std::cerr << "Alpha's Virtual Machine needs one (and only) input file"
+              << std::endl;
+    return 2;
+
+  } else if (!argv[1]) {
+    std::cerr << "Virtual machine's `main` function argument failure"
+              << std::endl;
+    return 3;
+  }
+
   Memory mem;
   Loader loader(mem.consts, mem.code);
 
   try {
-    loader.load_from("tcode.abc");
+    loader.load_from(argv[1]);
   } catch (std::runtime_error e) {
     std::cerr << e.what() << std::endl;
 
