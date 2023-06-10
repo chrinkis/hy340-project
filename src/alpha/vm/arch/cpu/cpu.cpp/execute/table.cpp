bf5054661c9dddd35c75a3d34baf6d61e6ad5adf
ALEXANDROS CHRISTINAKIS <csd4764@csd.uoc.gr>
2023-06-10 18:15:11 +0300

    feat(vm): implement `Cpu::execute_newtable`

--- src/alpha/vm/arch/cpu/cpu.cpp/execute/table.cpp
+++ src/alpha/vm/arch/cpu/cpu.cpp/execute/table.cpp
@@ -2,10 +2,19 @@
 
 #include <utils/warnings.h>
 
+using RunTimeTable = alpha::vm::runtime::table::Table;
+
 namespace alpha::vm::arch::cpu {
 
 void Cpu::execute_newtable(const AbcInstruction& instr) {
-  WARN_EMPTY_FUNC_IMPL();
+  mem::Cell& lv = this->translate_arg_to_cell(instr.get_result());
+
+  FIXME;  // add missing assert
+
+  lv.clear();
+
+  lv = mem::Cell::for_table(RunTimeTable::create());
+  lv.get_table().increase_counter();
 }
 
 void Cpu::execute_tablegetelem(const AbcInstruction& instr) {
